c0d9fc52b80c6ce28970eaed35ab66ee3a40985b
Suprim Rijal <suprimrijal1@gmail.com>
2026-08-29 20:54:00 +0300

    React Lab5

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
