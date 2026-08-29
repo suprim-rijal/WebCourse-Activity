85376b090d62022ea891b003ac949dadab34f23b
Suprim Rijal <suprimrijal1@gmail.com>
2026-08-29 20:53:03 +0300

    Lab 4

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 React Lab</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to React Lab</h1>
+    </header>
+  );
+}
+
+export default Header;
